516444c1ece963f24bdea914b97f7be4eb931057
Joanna Szaban <joannaszaban@MacBook-Pro-Joanna.local>
2023-03-14 12:11:26 +0100

    first commit

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Joanna Szaban on 10/09/2022.
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
